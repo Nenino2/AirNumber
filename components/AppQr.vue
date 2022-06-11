412e0c00badca4b8517bdf80d9079e0b0a7c1fbd
Alberto <42894888+Nenino2@users.noreply.github.com>
2022-06-11 17:09:54 +0200

    Added vcard

--- components/AppQr.vue
+++ components/AppQr.vue
@@ -5,16 +5,21 @@
 <script lang="ts">
 import { Vue } from 'vue-property-decorator';
 import QRCode from "qrcode";
+import VCard from 'vcard-creator'
 
 export default class Home extends Vue {
-    name = localStorage.getItem("airnumber_name") || ""
-    surname = localStorage.getItem("airnumber_surname") || ""
-    phoneNumber = localStorage.getItem("airnumber_phonenumber") || ""
+    name = localStorage.getItem("airnumber_name") || "air"
+    surname = localStorage.getItem("airnumber_surname") || "number"
+    phoneNumber = localStorage.getItem("airnumber_phonenumber") || "123456789"
 
     mounted() {
+        const personVCard = new VCard()
+
+        personVCard.addName(this.surname, this.name).addPhoneNumber(this.phoneNumber);
+
         const canvasElement = this.$refs.canvas;
 
-        QRCode.toCanvas(canvasElement, this.name+this.surname+this.phoneNumber, (error) => {
+        QRCode.toCanvas(canvasElement, personVCard.toString(), (error) => {
             if (error) console.error(error)
             console.log('success!');
         })
